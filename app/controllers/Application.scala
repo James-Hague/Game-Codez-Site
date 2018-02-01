02b2c66238d3066331ce9d407856fcba2a43382c
Kulca93 <Lee.Kulcar@gmail.com>
2018-02-01 14:13:04 +0000

    bootstrap2

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,21 +1,37 @@
 package controllers
-
+import scala.collection.mutable.ListBuffer
 import play.api._
 import play.api.mvc._
 
 
 class Application extends Controller {
 
+  var gameInfoList = new ListBuffer[String]()
+
+
+
   def index = Action {
     Ok(views.html.index("ITS WORKING."))
   }
-def Homepage = Action {
 
-  Ok("Game-Codez-Homepage")
+  def Homepage = Action {
+    Ok(views.html.Homepage("Game-Codez-Homepage"))
+  }
 
+  def GamePage = Action {
+    Ok(views.html.GamePage("Game-Codez-Homepage"))
+  }
 
+  def LoginPage = Action {
+    Ok(views.html.LoginPage("Game-Codez-Homepage"))
+  }
 
-
+def MembersPage = Action {
+  Ok(views.html.MembersPage("Game-Codez-Homepage"))
 }
+  def Fallout4Page = Action {
+    Ok(views.html.Fallout4Page("Game-Codez-Homepage"))
+  }
+
 
 }
